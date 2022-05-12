09e098e400518d1182293110a1ff7d9d0e18c7e9
Tyler Wells <tyler.f.wells@gmail.com>
2022-05-12 11:10:15 -0300

    user register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import LogInPage from "./components/LogInPage/LogInPage";
+import UserRegisterPage from "./components/UserRegisterPage/UserRegisterPage";
+import VendorRegisterPage from "./components/VendorRegisterPage/VendorRegisterPage";
 import UserSideBar from "./components/UserSideBar/UserSideBar";
 
 function App() {
   return (
     <div>
+      <UserRegisterPage />
       {/* <LogInPage /> */}
-      <UserSideBar/>
+      {/* <VendorRegisterPage /> */}
+      {/* <UserSideBar /> */}
     </div>
   );
 }
