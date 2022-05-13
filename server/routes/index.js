ea36ad1f6a4256930d35868baa5e18378ee0f587
mkcannon <michaelkaib@rocketmail.com>
2022-05-13 12:48:27 -0400

    login & register routes functional

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,50 +1,47 @@
-const { Router } = require('express');
+const { Router } = require("express");
 //const authMiddleware = require('../middleware/auth')
-const user = require('../controllers/User');
-const vendor = require('../controllers/Vendor');
-const reservation = require('../controllers/Reservation');
+const user = require("../controllers/User");
+const vendor = require("../controllers/Vendor");
+const reservation = require("../controllers/Reservation");
 const router = Router();
 
 // USER
 // Register user
-router.post('/user/register', user.registerUser)
+router.post("/user/register", user.registerUser);
 
 // User login
-router.post('/user/login', user.userLogin);
+router.post("/user/login", user.userLogin);
 
-// Get user by id
-router.get('/user/:id', user.getUser);
+// Get user by id for profile info
+router.get("/user/:id", user.getUser);
 
 // Get all users
-router.get('/users', user.getAllUsers);
+router.get("/users", user.getAllUsers);
 
 // Get user reservations
-router.get('/user/reservations/:id', user.getUserReservations);
-
+router.get("/user/reservations/:id", user.getUserReservations);
 
 // VENDOR
 // Create vendor
-router.post('/vendor/register', vendor.registerVendor);
+router.post("/vendor/register", vendor.registerVendor);
 
 // Vendor Login
-router.post('/vendor/login', vendor.vendorLogin);
+router.post("/vendor/login", vendor.vendorLogin);
 
 // Get vendor by id
-router.get('/vendor/:id', vendor.vendorLogin);
+router.get("/vendor/:id", vendor.vendorLogin);
 
 // Get all vendors
-router.get('/vendors', vendor.getAllVendors)
+router.get("/vendors", vendor.getAllVendors);
 
 // vendor reservations
-router.get('/vendor/reservation/:id', vendor.getVendorReservations);
-
+router.get("/vendor/reservation/:id", vendor.getVendorReservations);
 
 // RESERVATIONS
 // Create reservation
-router.post('/reservations', () => {});
+router.post("/reservations", () => {});
 
 // Delete reservation
-router.delete('/reservations/:id', () => {});
-
+router.delete("/reservations/:id", () => {});
 
 exports.router = router;
