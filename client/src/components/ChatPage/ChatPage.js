1ba7016f78dbf91264fecdf6ccf021d1c4e04aa4
Tyler Wells <tyler.f.wells@gmail.com>
2022-05-14 17:35:13 -0300

    commiting the chat page
    before pulling new stuff

--- client/src/components/ChatPage/ChatPage.js
+++ client/src/components/ChatPage/ChatPage.js
@@ -1,12 +1,41 @@
 import React from "react";
 import UserSideBar from "../UserSideBar/UserSideBar";
-import SecondSideBar from "../SecondSideBar/SecondSideBar";
+import MyChatBubble from "../MyChatBubble/MyChatBubble";
+import OtherChatBubble from "../OtherChatBubble/OtherChatBubble";
 import styles from "./chatPage.module.css";
+import logo from "../../assets/images/clipart1129793.png";
+import sendButton from "../../assets/images/send.png";
 
 export default function ChatPage() {
   return (
-    <div className={styles.chatContainer}>
-      <SecondSideBar />
+    <div className={styles.overallContainer}>
+      <UserSideBar />
+      <div className={styles.chatContainer}>
+        <header className={styles.headerContainer}>
+          <div className={styles.chatNameContainer}>
+            <img src={logo} className={styles.chatPicture} />
+            <span className={styles.chatMember}>Ammar Ayeyo </span>
+          </div>
+        </header>
+        <div className={styles.chatInteractionContainer}>
+          <div className={styles.chatContainerBlackCover}>
+            <div className={styles.chatContent}>
+              <OtherChatBubble msg="Aye yooooo" />
+              <OtherChatBubble msg="this chat page aint it" />
+              <MyChatBubble msg="man shutup man, florida men stand up" />
+              <OtherChatBubble msg="what fam???" />
+              <MyChatBubble msg="Florida>Queens" />
+              <MyChatBubble msg="FACTS" />
+            </div>
+            <div className={styles.chatInputContainer}>
+              <input className={styles.messageInput}></input>
+              <button type="submit" className={styles.sendButton}>
+                <img src={sendButton} className={styles.sendIcon} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
