6a98b7aa718783695267a414bd4d8cd0e624058a
mkcannon <michaelkaib@rocketmail.com>
2022-05-14 19:36:11 -0400

    commented out password verification

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,8 +1,8 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../database/models/User");
-const Reservation = require('../database/models/Reservation');
-const Vendor = require('../database/models/Vendor');
+const Reservation = require("../database/models/Reservation");
+const Vendor = require("../database/models/Vendor");
 const SECRET_KEY = process.env.SECRET_KEY;
 
 // USER REGISTER
@@ -23,13 +23,23 @@ exports.registerUser = async (req, res) => {
     }
     if (password === "") throw new Error();
     const hash = await bcrypt.hash(password, 10);
-    const storedUser = await User.create({userName, email, password: hash, company, userImg});
-    const accessToken = jwt.sign({
-      id: storedUser.id,
-      userName: storedUser.userName,
-      email: storedUser.email,
-      company: storedUser.company,
-    }, SECRET_KEY, { expiresIn: '2h'});
+    const storedUser = await User.create({
+      userName,
+      email,
+      password: hash,
+      company,
+      userImg,
+    });
+    const accessToken = jwt.sign(
+      {
+        id: storedUser.id,
+        userName: storedUser.userName,
+        email: storedUser.email,
+        company: storedUser.company,
+      },
+      SECRET_KEY,
+      { expiresIn: "2h" }
+    );
     return res.status(201).send({ accessToken, success: true });
   } catch (error) {
     console.log("ERROR", error);
@@ -42,49 +52,49 @@ exports.userLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email: email } });
-    const validatedPass = await bcrypt.compare(password, user.password);
-    if (!validatedPass) throw new Error();
-    const accessToken = jwt.sign({
-      id: user.id,
-      userName: user.userName,
-      email: user.email,
-      company: user.company,
-      userImg: user.userImg
-    }, SECRET_KEY, { expiresIn: '2h'});
+    // UNCOMMENT OUT WHEN NOT USING MOCK DATA
+    // const validatedPass = await bcrypt.compare(password, user.password);
+    // if (!validatedPass) throw new Error();
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        userName: user.userName,
+        email: user.email,
+        company: user.company,
+        userImg: user.userImg,
+      },
+      SECRET_KEY,
+      { expiresIn: "2h" }
+    );
     return res.status(200).send({ accessToken, success: true });
   } catch (error) {
-    console.log(error);
     return res
       .status(400)
-      .send({ error: "401", message: "Username or password is incorrect" });
+      .send({ error, message: "Username or password is incorrect" });
   }
 };
 
 // GET USER PROFILE
 exports.getUserProfile = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await User.findByPk(id);
-        delete user.dataValues.password;
-        return res.status(200).send(user);
-    } catch (error) {
-        return res.status(404).send({ error, message: 'Resource not found' });
-    }
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    delete user.dataValues.password;
+    return res.status(200).send(user);
+  } catch (error) {
+    return res.status(404).send({ error, message: "Resource not found" });
+  }
 };
-
 
 // GET ALL USERS
 exports.getAllUsers = async (req, res) => {
-    try {
-        const users = await User.findAll();
-        //console.log('USERS', users);
-        return res.status(200).json(users);
-    } catch (error) {
-        console.error('ERROR', error);
-        return res.status(500).send({res: 'Internal server error', error: true});
-    }
+  try {
+    const users = await User.findAll();
+    //console.log('USERS', users);
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send({ res: "Internal server error", error: true });
+  }
 };
 
-exports.userLogout = (req, res) => {
-
-};
+exports.userLogout = (req, res) => {};
--- server/controllers/Vendor.js
+++ server/controllers/Vendor.js
@@ -1,84 +1,100 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const Vendor = require('../database/models/Vendor');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Vendor = require("../database/models/Vendor");
 const SECRET_KEY = process.env.SECRET_KEY;
 
 // REGISTER
 exports.registerVendor = async (req, res) => {
-    try {
-    const { businessName, email, password, streetAddress, city, state, zipCode, maxCapacity, vendorImg  } = req.body;
+  try {
+    const {
+      businessName,
+      email,
+      password,
+      streetAddress,
+      city,
+      state,
+      zipCode,
+      maxCapacity,
+      vendorImg,
+    } = req.body;
     const vendor = await Vendor.findOne({ where: { email: email } });
     if (vendor) {
-        return res.status(409).send({ error: '409', message: 'Vendor already exists' });
+      return res
+        .status(409)
+        .send({ error: "409", message: "Vendor already exists" });
     }
-        if (password === '') throw new Error();
-        const hash = await bcrypt.hash(password, 10);
-        const newVendor = new Vendor({
-            ...req.body,
-            password: hash,
-        });
-        const { _id } = await newVendor.save();
-        const accessToken = jwt.sign({ _id }, SECRET_KEY);
-        res.status(201).send({ accessToken });
-    } catch (error) {
-        console.log('ERROR', error);
-        res.status(400).send({ error, message: 'Could not create new vendor' });
-    }
+    if (password === "") throw new Error();
+    const hash = await bcrypt.hash(password, 10);
+    const newVendor = new Vendor({
+      ...req.body,
+      password: hash,
+    });
+    const { _id } = await newVendor.save();
+    const accessToken = jwt.sign({ _id }, SECRET_KEY);
+    return res.status(201).send({ accessToken });
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ error, message: "Could not create new vendor" });
+  }
 };
 
 // LOGIN
 exports.vendorLogin = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const vendor = await Vendor.findOne({ where: { email: email } });
-        const validatedPass = await bcrypt.compare(password, vendor.password);
-        if (!validatedPass) throw new Error();
-        const accessToken = jwt.sign({ _id: vendor._id }, SECRET_KEY);
-        res.status(200).send({ accessToken, success: true });
-    } catch (error) {
-        console.log('ERROR', error);
-        res.status(400).send({ error: '401', message: 'Username or password is incorrect' });
-    }
+  const { email, password } = req.body;
+  try {
+    const vendor = await Vendor.findOne({ where: { email: email } });
+    //UNCOMMENT WHEN NOT USING MOCK DATA
+    // const validatedPass = await bcrypt.compare(password, vendor.password);
+    // if (!validatedPass) throw new Error();
+    const accessToken = jwt.sign({ _id: vendor._id }, SECRET_KEY);
+    return res.status(200).send({ accessToken, success: true });
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ error, message: "Username or password is incorrect" });
+  }
 };
 
 // GET VENDOR PROFILE BY ID
 exports.getVendorProfile = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const vendor = await Vendor.findByPk(id);
-        delete vendor.dataValues.password;
-        res.status(200).send(vendor);
-    } catch (error) {
-        res.status(404).send({ error, message: 'Vendor not found' });
-    }
+  try {
+    const { id } = req.params;
+    const vendor = await Vendor.findByPk(id);
+    delete vendor.dataValues.password;
+    return res.status(200).send(vendor);
+  } catch (error) {
+    return res.status(404).send({ error, message: "Vendor not found" });
+  }
 };
 
 // GET ALL VENDORS
 exports.getAllVendors = async (req, res) => {
-    try {
-      const vendors = await Vendor.findAll();
-      return res.status(200).json(vendors);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).send({res: 'Internal server error', error: true});
-    }
+  try {
+    const vendors = await Vendor.findAll();
+    return res.status(200).json(vendors);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ res: "Internal server error", error: true });
+  }
 };
 
 // GET REGISTRATIONS
 exports.getVendorReservations = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const vendor = await Vendor.findOne({ where: {id: id},
-            include: {
-                model: Reservation,
-                as: 'vendorReserve',
-                attributes: ['reserveDate', 'partySize']
-            },
-            attributes: ['vendorName', 'address']
-        });
-        return res.status(200).json(vendor);
-    } catch (error) {
-        console.error(error, 'in controllers');
-        return res.status(500).send({res: 'Internal server error', error: true});
-    }
+  try {
+    const id = req.params.id;
+    const vendor = await Vendor.findOne({
+      where: { id: id },
+      include: {
+        model: Reservation,
+        as: "vendorReserve",
+        attributes: ["reserveDate", "partySize"],
+      },
+      attributes: ["vendorName", "address"],
+    });
+    return res.status(200).json(vendor);
+  } catch (error) {
+    console.error(error, "in controllers");
+    return res.status(500).send({ res: "Internal server error", error: true });
+  }
 };
