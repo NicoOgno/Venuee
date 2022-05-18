abb589234035647b4fa6d81fd162ceb3f6a530d9
Ammar <ammarkhomusi@gmail.com>
2022-05-18 12:49:59 -0400

    materialui implementation
    inside venue search bar

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import VendorChat from "./Pages/VendorPages/VendorChat/VendorChat";
 import VendorChatFull from "./Pages/VendorPages/VendorChatFull/VendorChatFull";
 import VendorProfileInfo from "./Pages/VendorPages/VendorProfileInfo/VendorProfileInfo";
 import Login from "./Pages/UserPages/Login/Login";
+import VenueSearchBar from "./components/VenueSearchBar/VenueSearchBar";
 
 const noCurrentUser = {
   id: "",
@@ -41,35 +42,36 @@ export const VendorContext = React.createContext();
 
 function App() {
   return (
-    <UserContext.Provider value={noCurrentUser}>
-      <VendorContext.Provider value={noCurrentVendor}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/userRegister" element={<UserRegisterPage />} />
-            <Route path="/vendorRegister" element={<VendorRegisterPage />} />
-            <Route path="/userSearch" element={<UserSearch />} />
-            <Route path="/searchResults" element={<UserSearchResults />} />
-            <Route path="/userReservations" element={<UserReservations />} />
-            <Route path="/userChat" element={<UserChat />} />
-            <Route path="/userProfileInfo" element={<UserProfileInfo />} />
-            <Route path="/userChatFull" element={<UserChatFull />} />
-            <Route
-              path="/vendorReservations"
-              element={<VendorReservations />}
-            />
-            <Route
-              path="/vendorAvailability"
-              element={<VendorAvailability />}
-            />
-            <Route path="/vendorChat" element={<VendorChat />} />
-            <Route path="/vendorChatFull" element={<VendorChatFull />} />
-            <Route path="/vendorProfileInfo" element={<VendorProfileInfo />} />
-          </Routes>
-        </Router>
-      </VendorContext.Provider>
-    </UserContext.Provider>
+    // <UserContext.Provider value={noCurrentUser}>
+    //   <VendorContext.Provider value={noCurrentVendor}>
+    //     <Router>
+    //       <Routes>
+    //         <Route path="/" element={<Login />} />
+    //         <Route path="/userRegister" element={<UserRegisterPage />} />
+    //         <Route path="/vendorRegister" element={<VendorRegisterPage />} />
+    //         <Route path="/userSearch" element={<UserSearch />} />
+    //         <Route path="/searchResults" element={<UserSearchResults />} />
+    //         <Route path="/userReservations" element={<UserReservations />} />
+    //         <Route path="/userChat" element={<UserChat />} />
+    //         <Route path="/userProfileInfo" element={<UserProfileInfo />} />
+    //         <Route path="/userChatFull" element={<UserChatFull />} />
+    //         <Route
+    //           path="/vendorReservations"
+    //           element={<VendorReservations />}
+    //         />
+    //         <Route
+    //           path="/vendorAvailability"
+    //           element={<VendorAvailability />}
+    //         />
+    //         <Route path="/vendorChat" element={<VendorChat />} />
+    //         <Route path="/vendorChatFull" element={<VendorChatFull />} />
+    //         <Route path="/vendorProfileInfo" element={<VendorProfileInfo />} />
+    //       </Routes>
+    //     </Router>
+    //   </VendorContext.Provider>
+    // </UserContext.Provider>
     // <div>
+    <VenueSearchBar />
     //   {/* <UserRegisterPage /> */}
     //   {/* <LogInPage /> */}
     //   {/* <VendorRegisterPage /> */}
