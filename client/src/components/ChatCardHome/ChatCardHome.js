e6e3bc51f24ce66340d3577a4091c56497d88341
Tyler Wells <tyler.f.wells@gmail.com>
2022-05-18 10:18:47 -0300

    including the messages into the cards for messages

--- client/src/components/ChatCardHome/ChatCardHome.js
+++ client/src/components/ChatCardHome/ChatCardHome.js
@@ -3,6 +3,11 @@ import styles from "./chatCardHome.module.css";
 import VendorSideBar from "../VendorSidebar/VendorSidebar";
 import ChatCard from "../ChatCard/ChatCard";
 import ChatCardLast from "../ChatCardLast/ChatCardLast";
+import morgan from "../../assets/images/10007447_1405252279743343_829523453_n.jpg";
+import jack from "../../assets/images/jack.jpeg";
+import elon from "../../assets/images/elon.jpeg";
+import peter from "../../assets/images/thiel.jpeg";
+import sheryl from "../../assets/images/sheryl.jpeg";
 
 export default function ChatCardHome() {
   return (
@@ -11,26 +16,35 @@ export default function ChatCardHome() {
       <div className={styles.homeContainer}>
         <div className={styles.cardsContainer}>
           <ChatCard
-            name="Mike Freeman"
-            msg="duuuuuuuvvvvaaaallllll"
-            time="8:34 PM"
+            name="Morgan Freeman"
+            msg="Them: I'm nominated for another Oscar and need this venue for my party"
+            time="6:55 PM"
+            img={morgan}
           />
           <ChatCard
-            name="Ammar Freeman"
-            msg="pauuuuusssseeee the bmt"
-            time="8:34 PM"
+            name="Sheryl Sandberg"
+            msg="Them: Do you have capacity for our 72,000 employees?"
+            time="1:34 PM"
+            img={sheryl}
           />
           <ChatCard
-            name="Tyler Freeman"
-            msg="portlandia babyeeeee"
-            time="5:50 PM"
+            name="Jack Dorsey"
+            msg="Them: Bitcoin convention incoming legggooooo!"
+            time="11:50 AM"
+            img={jack}
           />
           <ChatCard
-            name="Ash Freeman"
-            msg="florida stand uuuup"
-            time="5:50 PM"
+            name="Elon Musk"
+            msg="Them: Joe and I want to do a podcast here lolz!"
+            time="9:50 PM"
+            img={elon}
           />
-          <ChatCardLast name="Nico Freeman" msg="neneeee" time="5:12 PM" />
+          <ChatCardLast
+            name="Peter Thiel"
+            msg="Them: Will you accept Ayn Randian Libertarian MAGA conventions?"
+            time="5:08 AM"
+            img={peter}
+          />
         </div>
       </div>
     </div>
