92dcde5189422b166971e20505385d3518d7d372
Tyler Wells <tyler.f.wells@gmail.com>
2022-05-18 23:14:38 -0300

    including SearchResultsCard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,47 @@
-
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UserReservations from './Pages/UserPages/UserReservations/UserReservations';
-import UserChat from './Pages/UserPages/UserChat/UserChat';
-import UserSearch from './Pages/UserPages/UserSearch/UserSearch';
-import UserSearchResults from './Pages/UserPages/UserSearchResults/UserSearchResults';
-import UserProfileInfo from './Pages/UserPages/UserProfileInfo/UserProfileInfo';
-import UserChatFull from './Pages/UserPages/UserChatFull/UserChatFull';
-import UserRegisterPage from './components/UserRegisterPage/UserRegisterPage';
-import VendorRegisterPage from './components/VendorRegisterPage/VendorRegisterPage';
-import VendorReservations from './Pages/VendorPages/VendorReservations/VendorReservations';
-import VendorAvailability from './Pages/VendorPages/VendorAvailability/VendorAvailability';
-import VendorChat from './Pages/VendorPages/VendorChat/VendorChat';
-import VendorChatFull from './Pages/VendorPages/VendorChatFull/VendorChatFull';
-import VendorProfileInfo from './Pages/VendorPages/VendorProfileInfo/VendorProfileInfo';
-import Login from './Pages/UserPages/Login/Login';
-import ChatCardHomeUser from './components/ChatCardHomeUser/ChatCardHomeUser';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserReservations from "./Pages/UserPages/UserReservations/UserReservations";
+import UserChat from "./Pages/UserPages/UserChat/UserChat";
+import UserSearch from "./Pages/UserPages/UserSearch/UserSearch";
+import UserSearchResults from "./Pages/UserPages/UserSearchResults/UserSearchResults";
+import UserProfileInfo from "./Pages/UserPages/UserProfileInfo/UserProfileInfo";
+import UserChatFull from "./Pages/UserPages/UserChatFull/UserChatFull";
+import UserRegisterPage from "./components/UserRegisterPage/UserRegisterPage";
+import VendorRegisterPage from "./components/VendorRegisterPage/VendorRegisterPage";
+import VendorReservations from "./Pages/VendorPages/VendorReservations/VendorReservations";
+import VendorAvailability from "./Pages/VendorPages/VendorAvailability/VendorAvailability";
+import VendorChat from "./Pages/VendorPages/VendorChat/VendorChat";
+import VendorChatFull from "./Pages/VendorPages/VendorChatFull/VendorChatFull";
+import VendorProfileInfo from "./Pages/VendorPages/VendorProfileInfo/VendorProfileInfo";
+import Login from "./Pages/UserPages/Login/Login";
+import ChatCardHomeUser from "./components/ChatCardHomeUser/ChatCardHomeUser";
 // import Calendar from './components/Calendar/Calendar';
-import VenueSearchBar from './components/VenueSearchBar/VenueSearchBar';
+import VenueSearchBar from "./components/VenueSearchBar/VenueSearchBar";
+
+import SearchResultsCard from "./components/SearchResultsCard/SearchResultsCard";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/userRegister" element={<UserRegisterPage />} />
-        <Route path="/vendorRegister" element={<VendorRegisterPage />} />
-        <Route path="/userSearch" element={<UserSearch />} />
-        <Route path="/searchResults" element={<UserSearchResults />} />
-        <Route path="/userReservations" element={<UserReservations />} />
-        <Route path="/userChat" element={<UserChat />} />
-        <Route path="/userProfileInfo" element={<UserProfileInfo />} />
-        <Route path="/userChatFull" element={<UserChatFull />} />
-        <Route path="/vendorReservations" element={<VendorReservations />} />
-        <Route path="/vendorAvailability" element={<VendorAvailability />} />
-        <Route path="/vendorChat" element={<VendorChat />} />
-        <Route path="/vendorChatFull" element={<VendorChatFull />} />
-        <Route path="/vendorProfileInfo" element={<VendorProfileInfo />} />
-      </Routes>
-    </Router>
+    <SearchResultsCard />
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/userRegister" element={<UserRegisterPage />} />
+    //     <Route path="/vendorRegister" element={<VendorRegisterPage />} />
+    //     <Route path="/userSearch" element={<UserSearch />} />
+    //     <Route path="/searchResults" element={<UserSearchResults />} />
+    //     <Route path="/userReservations" element={<UserReservations />} />
+    //     <Route path="/userChat" element={<UserChat />} />
+    //     <Route path="/userProfileInfo" element={<UserProfileInfo />} />
+    //     <Route path="/userChatFull" element={<UserChatFull />} />
+    //     <Route path="/vendorReservations" element={<VendorReservations />} />
+    //     <Route path="/vendorAvailability" element={<VendorAvailability />} />
+    //     <Route path="/vendorChat" element={<VendorChat />} />
+    //     <Route path="/vendorChatFull" element={<VendorChatFull />} />
+    //     <Route path="/vendorProfileInfo" element={<VendorProfileInfo />} />
+    //   </Routes>
+    // </Router>
     // <Calendar />
 
     // <div>
---
+++ client/src/components/SearchResultsCard/SearchResultsCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./searchResultsCard.module.css";
+import six from "../../assets/images/6.jpg";
+
+export default function SearchResultsCard() {
+  return (
+    <div className={styles.cardContainer}>
+      <img src={six} className={styles.cardImg}></img>
+      <div className={styles.infoContainer}>
+        <span className={styles.venueName}>Blue's Cat Cafe</span>
+        <span className={styles.venueType}>Restaurant</span>
+      </div>
+    </div>
+  );
+}
